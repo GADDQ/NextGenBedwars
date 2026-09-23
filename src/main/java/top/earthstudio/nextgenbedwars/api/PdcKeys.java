8c7f9b8326512323d0bb3b49f8ed7f17efedbb74
Earth_Studio <earthstudiomail@gmail.com>
2026-09-24 02:04:54 +0800

    Ban constructor for PdcKeys

--- src/main/java/top/earthstudio/nextgenbedwars/api/PdcKeys.java
+++ src/main/java/top/earthstudio/nextgenbedwars/api/PdcKeys.java
@@ -3,7 +3,10 @@ package top.earthstudio.nextgenbedwars.api;
 import org.bukkit.NamespacedKey;
 
 public final class PdcKeys {
+    private PdcKeys() {}
+
     public static final class spawner {
+        private spawner() {}
         public static final NamespacedKey drop = new NamespacedKey("nextgenbedwars", "spawner_drop");
         public static final NamespacedKey preventMerge = new NamespacedKey("nextgenbedwars", "prevent_merge");
     }
